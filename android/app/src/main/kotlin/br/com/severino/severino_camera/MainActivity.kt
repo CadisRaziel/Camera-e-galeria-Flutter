318937da4ccbdded01c7431561bac83d052b1687
CadisRaziel <vitorcsharp@hotmail.com>
2021-07-03 14:04:00 -0300

    iniciando app para usar a camera e pegar imagem da geleria

---
+++ android/app/src/main/kotlin/br/com/severino/severino_camera/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.severino.severino_camera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Imagens',
+      theme: ThemeData(        
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
